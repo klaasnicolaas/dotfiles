58174c7bbecdd37f60e10e48138a6b3b04a0a772
Klaas Schoute <klaas_schoute@hotmail.com>
2021-10-12 11:55:05 +0200

    Add docker to install script

--- install.sh
+++ install.sh
@@ -63,5 +63,26 @@ echo
 echo "** Installing NVM (nodejs)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
+#----------------------------------------------------------------------------
+# Docker
+#----------------------------------------------------------------------------
+echo
+echo "** Installing Docker"
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker $USER
+
+#----------------------------------------------------------------------------
+# Docker-compose
+#----------------------------------------------------------------------------
+echo
+echo "** Installing Docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo
 echo "** Done"
