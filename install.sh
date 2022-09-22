b2400387e5573ee044a00aad029be8462b2ea704
Klaas Schoute <klaas_schoute@hotmail.com>
2022-09-22 23:29:59 +0000

    ⬆️ Bump docker-compose to v2.11.1

--- install.sh
+++ install.sh
@@ -85,7 +85,7 @@ sudo usermod -aG docker $USER
 #----------------------------------------------------------------------------
 echo
 echo "** Installing Docker-compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 #----------------------------------------------------------------------------
