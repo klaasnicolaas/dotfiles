b69c1f2840492dbb4e7acff7347b7a3e76e6677b
Klaas Schoute <klaas_schoute@hotmail.com>
2024-04-03 22:16:35 +0200

    Add UV installation script

--- install.sh
+++ install.sh
@@ -57,6 +57,14 @@ libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
 #----------------------------------------------------------------------------
+# uv
+#----------------------------------------------------------------------------
+echo
+echo "** Installing UV"
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.cargo/env
+
+#----------------------------------------------------------------------------
 # nvm (nodejs)
 #----------------------------------------------------------------------------
 echo
