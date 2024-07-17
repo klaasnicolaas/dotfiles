cdfcef1e569930b1fb2f106c8d9dac3f87e12627
Klaas Schoute <hello@student-techlife.com>
2024-07-17 10:24:33 +0000

    Update ruby.sh to install libpq-dev dependency

--- components/ruby.sh
+++ components/ruby.sh
@@ -8,7 +8,7 @@ echo "** Installing Ruby"
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
 
 # Install ruby-build Ubuntu dependencies
-sudo -y apt install libyaml-dev
+sudo -y apt install libyaml-dev libpq-dev
 
 echo
 echo "-- Done --"
